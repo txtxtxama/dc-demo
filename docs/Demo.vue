6dd2ce83bac556a2d2dec9d8d726f66939174116
txtxtxama <2388105792@qq.com>
2024-03-30 23:26:22 +0800

    test

---
+++ docs/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-col gap-10px">
+        <ElInput v-model="content" type="textarea" :rows="5"></ElInput>
+        <div class="flex">
+            <ElButton @click="genAndDownload">生成txt并下载</ElButton>
+        </div>
+    </div>
+</template>
+
+<script lang="jsx" setup>
+import { downloadBlob } from '@/utils/blob'
+import { ElButton, ElInput } from 'element-plus'
+import { ref } from 'vue'
+
+const content = ref('')
+
+const genAndDownload = () => {
+    const blob = new Blob([content.value], { type: 'text/plain' })
+
+    downloadBlob(blob, `text-${new Date().getTime()}.txt`)
+
+    content.value = ''
+}
+</script>
+
+<style lang="scss" scoped></style>
